06641071f65e62178e8d98b4bde27f12857909e3
Strukov Vlad <sivistrukov@gmail.com>
2022-11-06 18:37:18 +0900

    Add product and category models

--- src/shop/models.py
+++ src/shop/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+PRODUCT_LABEL = [
+    'SALE',
+    'NEW',
+    'HOT',
+]
+
+
+class Category(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=50)
+    description = models.TextField(verbose_name='Описание', max_length=500)
+    slug = models.SlugField(verbose_name='Slug', max_length=50, unique=True, db_index=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, verbose_name='Товар', on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='Название', max_length=50)
+    description = models.TextField(verbose_name='Описание', max_length=500)
+    image = models.ImageField(verbose_name='Изображение', upload_to='media/products/')
+    price = models.IntegerField(verbose_name='Цена')
+    sale = models.IntegerField(verbose_name='Цена со скидкой', null=True)
+    label = models.CharField(verbose_name='Метка', max_length=5, default='NEW', choices=PRODUCT_LABEL, null=True)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return self.name
